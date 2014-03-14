7f55375ecbf2832b93a0f22902d1c465760491c5
Brendan Whitfield <brendanw@windworksdesign.com>
2014-03-14 17:36:52 -0400

    un php

--- REQ_HEAD.php
+++ REQ_HEAD.php
@@ -14,19 +14,19 @@
 		<meta name="keywords" content="portfolio, developer, programmer, programming, java, processing, python, actionscript, HTML, javascript, jquery, CSS, creative, interactive">
 		<title>Brendan Whitfield - Programmer &amp; Interactive Developer</title>
 
-		<link rel='shortcut icon' type='image/png' href='<?=$prefix?>favicon.png'>
+		<link rel='shortcut icon' type='image/png' href='favicon.png'>
 
 		<!-- CSS -->
-		<link rel='stylesheet' type='text/css' href='<?=$prefix?>css/reset.css'>
-		<link rel='stylesheet' type='text/css' href='<?=$prefix?>css/style.css'>
+		<link rel='stylesheet' type='text/css' href='css/reset.css'>
+		<link rel='stylesheet' type='text/css' href='css/style.css'>
 
 		<!-- JS -->
-		<script type="text/javascript" src='<?=$prefix?>js/jquery-1.9.1.min.js'></script>
-		<script type="text/javascript" src='<?=$prefix?>js/settings.js'></script>
-		<script type="text/javascript" src='<?=$prefix?>js/main.js'></script> <!-- init() -->
+		<script type="text/javascript" src='js/jquery-1.9.1.min.js'></script>
+		<script type="text/javascript" src='js/settings.js'></script>
+		<script type="text/javascript" src='js/main.js'></script> <!-- init() -->
 
 		<!--[if lt IE 9]>
 			<script src="js/html5shiv.js"></script>
-			<link rel='stylesheet' type='text/css' href='<?=$prefix?>css/ie.css'>
+			<link rel='stylesheet' type='text/css' href='css/ie.css'>
 		<![endif]-->
 		
