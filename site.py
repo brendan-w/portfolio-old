41804a092fa1b374cdfe2fc1f785875960e70d72
Brendan Whitfield <brendanw@windworksdesign.com>
2014-12-31 00:45:36 -0500

    got basic templating working

--- site.py
+++ site.py
@@ -1,18 +1,36 @@
 
+
+from pages import projects
+
+
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
+app.debug = True
 
 
+
+
+
+# static serving
 @app.route('/static/<path:filename>')
 def send_foo(filename):
     return send_from_directory('static/', filename)
 
 
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template('index.html')
+
+
+@app.route('/work/<project>')
+def project_page(project):
+    if project in projects:
+        return render_template('project.html', project=projects[project])
+    else:
+        return render_template('404.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
